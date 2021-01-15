a0e51709880ae5d452415f3356c73975103da0a9
cs-moudgill <channpreet.moudgill@gmail.com>
2021-01-14 23:52:46 -0400

    updations in product controller

--- routes/product.js
+++ routes/product.js
@@ -1,15 +1,26 @@
 var express = require("express");
 var router = express.Router();
 const { getUserById } = require("../controllers/user");
-const { getProductById, createProduct,getProduct,mediaHandling } = require("../controllers/product");
+const { getProductById, createProduct,getProduct,mediaHandling,updateProduct,deleteProduct,getAllProducts,updateStockSold } = require("../controllers/product");
 const { isLoggedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 
 //params
 router.param("userId", getUserById);
 router.param("productId", getProductById);
+
+//Create routes
 router.post("/product/create/:userId",isLoggedIn,isAuthenticated,isAdmin,createProduct);
+
+//Read routes
 router.get('/product/:productId',getProduct);
 router.get('/product/photo/:productId',mediaHandling);
 
+//Delete route
+router.delete('/product/:productId/:userId',isLoggedIn,isAuthenticated,isAdmin,deleteProduct);
+
+//Update route
+router.put('/product/:productId/:userId',isLoggedIn,isAuthenticated,isAdmin,updateProduct);
+
+router.get('/products',getAllProducts);
 
 module.exports = router;
